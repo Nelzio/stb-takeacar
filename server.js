3d43874a5b3ccb00168920654d2ad20d00bd773b
nelzio <nelziositoe@gmail.com>
2019-03-29 11:39:28 +0200

    config to deploy2

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const port = process.env.PORT || 5000
 const app = express()
 
 app.use(history())
-app.use(serveStatic(__dirname + '/dist/pwa-mat'))
+app.use(serveStatic(__dirname + '/dist/pwa'))
 app.listen(port)
