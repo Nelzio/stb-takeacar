d2dbf4e76d8d88e6b48f7e6d24626d407123baed
nelzio <nelziositoe@gmail.com>
2019-03-29 10:29:23 +0200

    to deploy

---
+++ dist/pwa/precache-manifest.df1ec7ea306340f5219d1507b5d47fa5.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d51d68f8f96a5b0594f",
+    "url": "css/062666cf.08d252b7.css"
+  },
+  {
+    "revision": "6d51d68f8f96a5b0594f",
+    "url": "js/062666cf.ec35fbdb.js"
+  },
+  {
+    "revision": "b3719c6511258d11774d",
+    "url": "css/29b455ee.2aa2da72.css"
+  },
+  {
+    "revision": "b3719c6511258d11774d",
+    "url": "js/29b455ee.731d6c6c.js"
+  },
+  {
+    "revision": "32d834bba6d2cf793ca0",
+    "url": "js/2d0c4e21.073dd117.js"
+  },
+  {
+    "revision": "89dec5cbb3635ac060ba",
+    "url": "js/4b47640d.b611ff6a.js"
+  },
+  {
+    "revision": "5a91c06eed10ac9c6f45",
+    "url": "css/app.e16d29be.css"
+  },
+  {
+    "revision": "5a91c06eed10ac9c6f45",
+    "url": "js/app.a19337b9.js"
+  },
+  {
+    "revision": "0f91b5762ab1caa4400c",
+    "url": "css/f59cca30.5d5f6c0e.css"
+  },
+  {
+    "revision": "0f91b5762ab1caa4400c",
+    "url": "js/f59cca30.e7d9384c.js"
+  },
+  {
+    "revision": "c9a07de1164fb351166b",
+    "url": "js/vendor.605d2e93.js"
+  },
+  {
+    "revision": "cf6613d1adf490972c557a8e318e0868",
+    "url": "fonts/KFOlCnqEu92Fr1MmWUlfBBc-.cf6613d1.woff"
+  },
+  {
+    "revision": "8c2ade503b34e31430d6c98aa29a52a3",
+    "url": "fonts/KFOlCnqEu92Fr1MmYUtfBBc-.8c2ade50.woff"
+  },
+  {
+    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
+    "url": "fonts/KFOmCnqEu92Fr1Mu4mxM.bafb105b.woff"
+  },
+  {
+    "revision": "a1471d1d6431c893582a5f6a250db3f9",
+    "url": "fonts/KFOlCnqEu92Fr1MmSU5fBBc-.a1471d1d.woff"
+  },
+  {
+    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
+    "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.e9dbbe8a.woff"
+  },
+  {
+    "revision": "de8b7431b74642e830af4d4f4b513ec9",
+    "url": "fonts/KFOlCnqEu92Fr1MmEU9fBBc-.de8b7431.woff"
+  },
+  {
+    "revision": "bca3a1873ac988faff0817eca96b2d86",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.bca3a187.woff2"
+  },
+  {
+    "revision": "9219a80f0478e0bfdee5f4c753ce8535",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.9219a80f.woff"
+  },
+  {
+    "revision": "efb00e57304380d857603760600da5d5",
+    "url": "statics/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f9d7587924933c1912c342b190b53ca",
+    "url": "statics/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "5988ed49bab6921d143bd8d6ef72d2a1",
+    "url": "statics/Imagens/huynday.png"
+  },
+  {
+    "revision": "3020c8ac2c2872dec7741e5948520093",
+    "url": "statics/icons/icon-128x128.png"
+  },
+  {
+    "revision": "da2b422e2895b9b9c1782a71b6be9d63",
+    "url": "statics/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "8c4120b4e88276badbc808e518572fad",
+    "url": "statics/icons/icon-192x192.png"
+  },
+  {
+    "revision": "20c6420ccf16a07d9d56538e30a6b524",
+    "url": "statics/icons/icon-256x256.png"
+  },
+  {
+    "revision": "ed12d10fd0407eea42ccaf7af7991146",
+    "url": "statics/icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "3020c8ac2c2872dec7741e5948520093",
+    "url": "statics/quasar-logo.png"
+  },
+  {
+    "revision": "66d7719a8ca03c1a591444c013802817",
+    "url": "statics/icons/icon-384x384.png"
+  },
+  {
+    "revision": "6825a83008b8237dced009b0f69bfcc0",
+    "url": "statics/Imagens/ford+grey.png"
+  },
+  {
+    "revision": "04142e4cd67cbe858d881e2c2aaf6f12",
+    "url": "statics/Imagens/semafuro.png"
+  },
+  {
+    "revision": "a13b2bd5261e2df374128df1aab93001",
+    "url": "statics/icons/icon-512x512.png"
+  },
+  {
+    "revision": "7a34f3eb64a2aa5361253f123ce59301",
+    "url": "statics/Imagens/toyota-logo.png"
+  },
+  {
+    "revision": "4e8ec6944deb525f2a38343b34dd4ecc",
+    "url": "statics/Imagens/semafuro.xcf"
+  },
+  {
+    "revision": "4cab443ad65b024fb5a9bef19497d5ff",
+    "url": "statics/Imagens/bmw.png"
+  },
+  {
+    "revision": "a6b772976a15437090b496bf766ef1db",
+    "url": "statics/Imagens/Jeep.png"
+  },
+  {
+    "revision": "0003dfb73239341814e0026a0d290453",
+    "url": "statics/Imagens/stbmz.png"
+  },
+  {
+    "revision": "4b82fce421a91102e5459d455849eeec",
+    "url": "index.html"
+  },
+  {
+    "revision": "64b89870527bc23a8f701d675020e1a5",
+    "url": "manifest.json"
+  }
+]);
